4ba9a85ac14862cc7336bdefae00c39759e2b5c9
Ilya <felagunt@live.ru>
2022-12-04 11:17:00 +0300

    gradle

--- app/src/main/java/com/example/newsapp/utils/Constants.kt
+++ app/src/main/java/com/example/newsapp/utils/Constants.kt
@@ -3,6 +3,6 @@ package com.example.newsapp.utils
 class Constants {
     companion object{
         const val BASE_URL = "https://newsapi.org/"
-        const val API_KEY = "91353fad465b4e38ada534c970935b18"
+        //const val API_KEY = ""
     }
 }
